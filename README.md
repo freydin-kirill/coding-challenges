093b6e9eb9bde70b4b2d3b9d6437526c3ff4f00d
Кирилл В Фрейдин <frejdin.kirill-v@net.ugatu.su>
2023-08-04 16:48:05 +0300

    Bracket Matcher

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
 # Coding Challenges
 
 **This repository contains solutions to online coding challenges from various websites. The coding problems solved so far are as follow:**
-- [x] 
+- [x] Tree Constructor: 
+- [x] Bracket Matcher:
+- [x]
+- [x]
+- [x]
+- [x]
+- [x]
---
+++ bracket_matcher/bracket_matcher.py
@@ -0,0 +1,26 @@
+def BracketMatcher(strParam):
+
+  # Initialize a counter to keep track of open parentheses '('
+  bracktes_counter = 0
+
+  # Loop through each character in the input string
+  for sym in strParam:
+    # If an opening parenthesis is encountered, increment the counter
+    if sym == '(':
+      bracktes_counter += 1
+    
+    # If a closing parenthesis is encountered, decrement the counter
+    if sym == ')':
+      bracktes_counter -= 1
+
+    # If the counter goes negative, it means there are more closing parentheses than 
+	# opening ones, which makes the parentheses unbalanced. So, return 0.
+    if bracktes_counter < 0:
+      return 0
+
+  # After processing the entire string, if the counter is 0, it means the parentheses are balanced.
+  # Return 1 (True) indicating balanced parentheses, otherwise, return 0 (False).
+  return 1 if bracktes_counter == 0 else 0
+
+# Keep this function call here 
+print(BracketMatcher(input()))
